314cf5416971bdfa406e5adc56a0bb222fc7f322
Vanina <vanina.tobar@gmail.com>
2019-09-01 22:24:41 -0400

    update firefox

--- src/test/java/selenium/finalDelivery/tests/AllTests.java
+++ src/test/java/selenium/finalDelivery/tests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ Test1.class, Test2.class, Test3.class, Test4.class, Test5.class })
+@SuiteClasses({ Test1.class, Test2.class })
 
 public class AllTests {
 
